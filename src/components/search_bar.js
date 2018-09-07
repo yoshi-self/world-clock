2e87bbc9a9332d17ece2586e149b1416f3997e45
Yoshi <crispy245@gmail.com>
2018-09-07 17:27:48 -0400

    Add error messages

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -4,11 +4,12 @@ import Autocomplete from 'react-autocomplete';
 import moment from 'moment-timezone';
 
 import {addTimezone} from '../actions/index';
+import './search_bar.css';
 
 class SearchBar extends Component{
   constructor(props) {
     super(props);
-    this.state = {name: ''};
+    this.state = {name: '', alertMessage: ''};
   }
 
   onFormSubmit(event) {
@@ -16,40 +17,66 @@ class SearchBar extends Component{
 
     // found: Boolean
     const found = moment.tz.names().includes(this.state.name);
-    const inList = this.props.timezones.includes(this.state.name);
-    if(found && !inList) {
+    if(!found) {
+      this.setState({
+        alertMessage: 'Time zone name does not exist'
+      });
+      return;
+    }
+
+    const isInList = this.props.timezones.includes(this.state.name);
+    if(isInList) {
+      this.setState({
+        alertMessage: 'Time zone is already in the list'
+      });
+      return;
+    }
+    if(found && !isInList) {
       this.props.addTimezone(this.state.name);
-      this.setState({name: ''});
+      this.setState({name: '', alertMessage: ''});
     }
   }
 
   render() {
+    let alertClass = 'search-alert';
+    if(this.state.alertMessage) {
+      alertClass += ' opacity-1';
+    }
+    else {
+      alertClass += ' opacity-0';
+    }
+
     return (
-      <form
-        className="input-group search-form"
-        onSubmit={this.onFormSubmit.bind(this)}
-      >
-        <Autocomplete
-          getItemValue={(item) => item}
-          items={moment.tz.names()}
-          shouldItemRender={(item, value) => {
-            return item.toLowerCase().includes(value.toLowerCase());
-          }}
-          renderItem={(item, isHighlighted) =>
-            <div key={item} style={{ background: isHighlighted ? 'lightgray' : 'white' }}>
-              {item}
-            </div>
-          }
-          value={this.state.name}
-          onChange={(event) => this.setState({name: event.target.value})}
-          onSelect={(value) => this.setState({name: value})}
-          inputProps={{
-            className: "form-control",
-            placeholder: "Time zone"
-          }}
-        />
-        <button type="submit" className="btn btn-primary">Add</button>
-      </form>
+      <div className="SearchBar clearfix">
+        <form
+          className="input-group search-form"
+          onSubmit={this.onFormSubmit.bind(this)}
+        >
+          <Autocomplete
+            getItemValue={(item) => item}
+            items={moment.tz.names()}
+            shouldItemRender={(item, value) => {
+              return item.toLowerCase().includes(value.toLowerCase());
+            }}
+            renderItem={(item, isHighlighted) =>
+              <div key={item} style={{ background: isHighlighted ? 'lightgray' : 'white' }}>
+                {item}
+              </div>
+            }
+            value={this.state.name}
+            onChange={(event) => this.setState({name: event.target.value})}
+            onSelect={(value) => this.setState({name: value})}
+            inputProps={{
+              className: "form-control",
+              placeholder: "Time zone"
+            }}
+          />
+          <button type="submit" className="btn btn-primary">Add</button>
+        </form>
+        <div className={alertClass}>
+          {this.state.alertMessage}
+        </div>
+      </div>
     );
   }
 }
